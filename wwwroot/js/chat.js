1ba185b83f47248e8258321b2940a00eb8f9ad8a
Or1onn <salaxetdinov.orxan@gmail.com>
2023-02-16 18:13:24 +0400

    Pre release

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -64,6 +64,22 @@ $(".new-chat-user").click(function () {
 
 document.getElementById("sendBtn").addEventListener("click", () => {
     const message = document.getElementById("message").value;
+    let date = new Date();
+    document.getElementById('messageList').insertAdjacentHTML(
+        'afterbegin',
+        `<div class="chat-message-container-your">
+                            <div class="chat-message-your">
+                                <div class="chatting-your-message-text">
+                                    ${message}
+                                </div>
+                                <span class="chat-your-message-time">${date.getHours() + ':' + date.getMinutes()}</span>
+                                <div class="message-kr">
+                                    <svg viewBox="0 0.65 8 13" height="30" width="30" preserveAspectRatio="xMidYMid meet" class="" version="1.1" x="0px" y="0px" enable-background="new 0 0 8 13" xml:space="preserve">
+                                    <path fill="#00a884" d="M5.188,1H0v11.193l6.467-8.625 C7.526,2.156,6.958,1,5.188,1z"></path>
+                  </svg>
+                                </div>
+                            </div>
+                        </div>`)
 
     let text = EncryptMessage(message);
 
@@ -72,17 +88,23 @@ document.getElementById("sendBtn").addEventListener("click", () => {
 });
 
 hubConnection.on("ReceiveMessage", (messages, privateKey) => {
-    messages.forEach(function (element) {
-        let date = new Date(element.createTime);
-        document.getElementById('messageList').insertAdjacentHTML(
-            'afterbegin',
-            `<div class="chatting-frnd-message">
-                            <p>
-                                ${DecryptMessage(element.text, privateKey)}
-                                <span>${date.getHours() + ':' + date.getMinutes()}</span>
-                            </p>
+    let date = new Date(element.createTime);
+    document.getElementById('messageList').insertAdjacentHTML(
+        'afterbegin',
+        `<div class="chat-message-container-frnd">
+                            <div class="chat-message-frnd">
+                                <div class="frnd-message-kr">
+                                    <svg viewBox="0 0.3 8 13" height="25" width="25" preserveAspectRatio="xMidYMid meet" class="" version="1.1" x="0px" y="0px" xml:space="preserve">
+                                    <path fill="#3b4a54" d="M1.533,3.568L8,12.193V1H2.812 C1.042,1,0.474,2.156,1.533,3.568z"></path>
+                  </svg>
+                                </div>
+                                <div class="chatting-frnd-message-text">
+                                    ${DecryptMessage(message, privateKey)}
+                                </div>
+                                <span class="chat-frnd-message-time">${date.getHours() + ':' + date.getMinutes()}</span>
+                            </div>
                         </div>`
-        )
-    });
+
+    )
 });
 
