94adbd49557ebff8465a8beb94f18ada2b63a6a4
Ashotch <muradlireceb@gmail.com>
2023-02-27 23:48:31 +0400

    Mobile button upd, login upd

--- Login_Window/main.js
+++ Login_Window/main.js
@@ -2,11 +2,11 @@ document.getElementById("eye").addEventListener("click", async () => {
     var pass = document.getElementById("pass");
     var eye = document.getElementById("eye");
     if (pass.type == "password") {
-        eye.src = "hide_pass.svg"
+        eye.src = "Icon/hide_pass.svg"
         pass.type = "text"
     }
     else {
-        eye.src = "show_pass.svg"
+        eye.src = "Icon/show_pass.svg"
         pass.type = "password"
     }
 });
